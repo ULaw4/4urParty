27ceee5c26342f5b636e80777417498debab52a3
JGMN <gabrielmozqueda04@hotmail.com>
2024-09-22 19:24:32 -0700

    boton de whatsapp

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -25,6 +25,23 @@ const path = [
 
 export default function Navbar() {
   return (
+    <>
+    <nav className="bg-white md:bg-neutral-400 py-4 shadow-sm sticky top-0 z-50 space-x-8 text-lg text-gray-700">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        {/* Logo */}
+        <div className="text-2xl font-bold text-gray-700 uppercase">
+          MARLENE
+        </div>
+        {/* Nav Links */}
+        <ul className="flex space-x-8 text-gray-400">
+          {path.map((item,k) => (
+            <li key={k} className="hover:text-gray-700">
+              <Link href={item.url}>{item.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
     <nav className="bg-white py-4 shadow-sm sticky top-0 z-50 space-x-8 text-lg text-gray-700">
       <div className="container mx-auto flex justify-between items-center px-4">
         {/* Logo */}
@@ -33,13 +50,15 @@ export default function Navbar() {
         </div>
         {/* Nav Links */}
         <ul className="flex space-x-8 text-gray-400">
-          {path.map((item) => (
-            <li className="hover:text-gray-700">
+          {path.map((item,k) => (
+            <li key={k} className="hover:text-gray-700">
               <Link href={item.url}>{item.name}</Link>
             </li>
           ))}
         </ul>
       </div>
     </nav>
+          </>
+          
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/navbar";
+import WhatsApp from "./components/whatsapp";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,10 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
         <Navbar />
         {children}
+        <WhatsApp />
       </body>
     </html>
   );
